e40b5a3f05a8b9191194603e8a72d0bc43253759
Эдуард Беришев <Clair911@outlook.com>
2021-10-17 14:02:29 +0700

    lb3 Изменения Добавил get set, изменил алгоритм ввода

--- LB3_sharp/Program.cs
+++ LB3_sharp/Program.cs
@@ -6,46 +6,83 @@ namespace LB3_sharp
     class Account
     {
         private double money;
-        private string name;
-        private string birthDate;
-        private int pin;
 
-        Account(string name, string birthDate,int pin,double money)
+        public Account(double money)
         {
-            this.name = name;
-            this.birthDate = birthDate;
-            this.pin = pin;
             this.money = money;
-            }
-
-        void withdraw(double money, int pin)
-        {
-            if (pin == this.pin)
-            {
-                this.money -= money;
-            }
-            else
-            {
-                Console.WriteLine("Неверный пин-код. Операция отменена");
-            }
         }
-        void deposit(double money, int pin)
+
+        public void withdraw(double money)
         {
-            if (pin == this.pin)
-            {
+                this.money -= money;
+        }
+        public void deposit(double money)
+        {
                 this.money += money;
-            }
-            else
-            {
-                Console.WriteLine("Неверный пинкод. Операция отменена");
-            }
+        }
+        public void show()
+        {
+            Console.WriteLine("Money: {0}",this.money);
+        }
+
+         public double moneyget()
+        {
+            return money;
+        }
+        public void moneyput(double _money)
+        {
+            money += _money;
         }
     }
     class Program
     {
+
+        static int splitLine(string str)
+        {
+            for (int i=0; i < str.Length; i++)
+            {
+                if (str[i] == ' ') return i;
+            }
+            return 0;
+        }
+
+
         static void Main(string[] args)
         {
+            Account user = new Account(0);
+            double money;
+            string oper;
+            int count;
+            while (true) {
+                Console.WriteLine("Введите тип операции и сумму (дробная часть отделяется запятой)");
+                oper = Console.ReadLine();
+                if (oper == "exit")
+                {
+                    Environment.Exit(0);
+                }
+                count = splitLine(oper);
+                money = Convert.ToDouble(oper.Substring(count + 1));
+                oper = oper.Substring(0, count);
+                switch (oper) {
+                    case "withdraw":
+                        //user.withdraw(money);
+                        //user.show();
+                        user.moneyput(-money);
+                        Console.WriteLine("Money: {0}",user.moneyget());
+                        break;
+                    case "deposit":
+                        //user.deposit(money);
+                        //user.show();
+                        user.moneyput(money);
+                        Console.WriteLine("Money: {0}", user.moneyget());
+                        break;
+                    default:
+                        Console.WriteLine("Такой операции не существует");
+                        break;
 
+
+                } 
+            }
         }
     }
 }
